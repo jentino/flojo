032543bf8b42cef64501ced8f2eadafa6e852c61
jenty <jentino@gmail.com>
2018-01-19 01:31:05 +0200

    3rd

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+//version 1.1.1
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
